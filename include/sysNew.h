f26ab138486a17ef1600e259a982bf1eae07fa5c
GameCubing8619 <145263194+GameCubing8619@users.noreply.github.com>
2026-03-16 03:43:41 -0700

    whoops

--- include/sysNew.h
+++ include/sysNew.h
@@ -5,10 +5,7 @@
 #include "types.h"
 #include <stddef.h>
 
-inline void* operator new(size_t size)
-{
-    System::alloc(size);
-}
+void* operator new(size_t size);
 void* operator new[](size_t size);
 void* operator new(size_t size, int alignment);
 void* operator new[](size_t size, int alignment);
